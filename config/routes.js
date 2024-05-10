1ea025094eb147290cbf088a1f8de61b8204cf59
Jerrell Boyer <jerrellbb@yahoo.co.uk>
2024-05-10 12:26:13 +0100

    basic backend plus test endpoint completed

---
+++ config/routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import {test} from '../controllers/paymentController.js'
+
+const router = express.Router()
+
+router.route('/test')
+  .get(test)
+
+
+export default router
---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import router from './config/routes.js'
+import 'dotenv/config' // only needs to be added if it doesn't already exist
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+//! Variables
+const app = express()
+const port = process.env.PORT
+const connectionString = process.env.CONNECTION_STRING
+//! Middleware 
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(`REQUEST RECEIVED: ${req.method} ${req.originalUrl}`)
+  next()
+})
+
+//! Endpoints
+app.use('/api', router)
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+// Start servers
+async function startServer() {
+  try {
+    // Establish a connection to the MongoDB database (Atlas)
+    await mongoose.connect(connectionString)
+    console.log('✅ Database connection established')
+    // Listen
+    app.listen(port, () => console.log(`🚀 Server listening on port ${port}`))
+    console.log(__filename)
+    console.log(__dirname)
+  } catch (error) {
+    console.log('🚨 Error connection')
+    console.log(error)
+  }
+}
+startServer()
